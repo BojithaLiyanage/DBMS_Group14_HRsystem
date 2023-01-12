e72565d1e3a460c971b004c9106f95b19955e68f
BojithaLiyanage <bojitharc@gmail.com>
2023-01-12 11:02:35 +0530

    commit-2

---
+++ BackEnd/routes/adminRoutes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const adminController = require("../controllers/adminController");
+const auth = require("../middleware/auth");
+
+const router = express.Router();
+
+// admin login post - postAdminLogin
+router.post("/", adminController.postAdminLogin);
+
+// HR manager add post - postAddHRM
+router.post("/addHR", auth.requireAuth, adminController.postAddHRM);
+
+// get HR manager details
+router.get("/getHRM", auth.requireAuth, adminController.getHRM);
+
+router.get("/getDetails", auth.requireAuth, adminController.getDetails);
+
+module.exports = router;
